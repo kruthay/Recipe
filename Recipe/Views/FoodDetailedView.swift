61bac717ab0f74bba2c53027047ae744c58870ff
Kruthay Kumar Reddy Donapati <kruthay@gmail.com>
2023-11-16 12:34:47 -0500

    Updated Detailed view to work with all screen sizes

--- Recipe/Views/FoodDetailedView.swift
+++ Recipe/Views/FoodDetailedView.swift
@@ -15,71 +15,73 @@ struct FoodDetailedView: View {
     var meal: Food
     @Environment(\.modelContext) private var modelContext
     var body: some View {
-        HStack {
-            Text(meal.name ?? "")
-                .font(.headline)
-                .fontWeight(.bold)
-            
-        }
-        List {
-            HStack {
-                Spacer()
-                AsyncImage(
-                    url: meal.thumbnail) {  phase in
-                        switch(phase) {
-                        case .success(let image):
-                            image.resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 200, maxHeight: 200)
-                        case .failure(_):
-                            
-                            Color.clear
-                                .frame(maxWidth: 200, maxHeight: 200)
-                        default :
-                            ProgressView()
-                                .frame(maxWidth: 200, maxHeight: 200)
-                        }
-                    }
-                Spacer()
-            }
-            
-            Section {
-                if let instructions  = meal.instructions {
-                    Text(instructions)
-                } else {
-                    ProgressView()
-                }
+        GeometryReader { geometry in
+            VStack {
+                Text(meal.name ?? "")
+                    .font(.headline)
+                    .fontWeight(.bold)
                 
-            } header: {
-                Text("Instructions")
-            }
-            Section {
-                ScrollView {
-                    if meal.ingredientsAndMeasurements.isEmpty {
-                        ProgressView()
-                    }
-                    else {
-                        ForEach(meal.ingredientsAndMeasurements.sorted(by: >), id: \.key) { key, value in
-                            HStack {
-                                Text(key)
-                                Spacer()
-                                Text(value)
+                List {
+                    HStack {
+                        Spacer()
+                        AsyncImage(
+                            url: meal.thumbnail) {  phase in
+                                switch(phase) {
+                                case .success(let image):
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: geometry.size.width/2, maxHeight: geometry.size.height/2)
+                                case .failure(_):
+                                    
+                                    Color.clear
+                                        .frame(maxWidth: geometry.size.width/2, maxHeight: geometry.size.height/2)
+                                default :
+                                    ProgressView()
+                                        .frame(maxWidth: geometry.size.width/2, maxHeight: geometry.size.height/2)
+                                }
                             }
-                            .padding()
+                        Spacer()
+                    }
+                    
+                    Section {
+                        if let instructions  = meal.instructions {
+                            Text(instructions)
+                        } else {
+                            ProgressView()
+                        }
+                        
+                    } header: {
+                        Text("Instructions")
+                    }
+                    Section {
+                        ScrollView {
+                            if meal.ingredientsAndMeasurements.isEmpty {
+                                ProgressView()
+                            }
+                            else {
+                                ForEach(meal.ingredientsAndMeasurements.sorted(by: >), id: \.key) { key, value in
+                                    HStack {
+                                        Text(key)
+                                        Spacer()
+                                        Text(value)
+                                    }
+                                    .padding()
+                                }
+                            }
+                        }
+                    } header: {
+                        Text("Ingredients")
+                    }
+                    
+                }
+                .padding()
+                .task {
+                    if meal.instructions == nil && meal.ingredientsAndMeasurements.isEmpty {
+                        let values = await FoodValuesCollection.getFullInfo(of: meal.id)
+                        withAnimation {
+                            (meal.instructions, meal.ingredientsAndMeasurements) = values
                         }
                     }
-                }
-            } header: {
-                Text("Ingredients")
-            }
-            
-        }
-        .padding()
-        .task {
-            if meal.instructions == nil && meal.ingredientsAndMeasurements.isEmpty {
-                let values = await FoodValuesCollection.getFullInfo(of: meal.id)
-                withAnimation {
-                    (meal.instructions, meal.ingredientsAndMeasurements) = values
                 }
             }
         }
@@ -91,5 +93,5 @@ struct FoodDetailedView: View {
 Just for the Preview purposes.
 Instructions are for the sample.
 """, ingredientsAndMeasurements: ["Milk":"100ML", "Powder":"10Oz"]))
-        .modelContainer(for: Food.self, inMemory: true)
+    .modelContainer(for: Food.self, inMemory: true)
 }
